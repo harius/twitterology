44af93602fdff81609e38d3bd7321b84d6b2afd7
Zakhar Pilipchuk <zahaaar@yandex-team.ru>
2016-03-25 10:43:45 +0300

    Support filtering by location

--- examples/save_some_tweets.py
+++ examples/save_some_tweets.py
@@ -1,18 +1,20 @@
+# Usage: python examples/save_some_tweets.py track     '#HelloWorld'
+#    Or: python examples/save_some_tweets.py locations '54.2,35.1,57.0,40.2'
 from sys import argv
 
 import twitterology as tw
 
 
 if __name__ == "__main__":
-    track = argv[1]
+    query = {argv[1]: argv[2]}
 
     client = tw.stream_client()
-    storage = tw.sources.tweets(track=track)
+    storage = tw.sources.tweets(**query)
 
     print "Writing to table", storage.table
     print "%%"
 
-    tweets = client.statuses.filter.post(track=track).stream()
+    tweets = client.statuses.filter.post(**query).stream()
     for tweet in tweets:
         try:
             print tweet["user"]["name"]
--- twitterology/sources.py
+++ twitterology/sources.py
@@ -3,13 +3,20 @@ from datetime import datetime
 import dataset
 
 
-def tweets(track=None, session=None):
+def tweets(track=None, locations=None, session=None):
     database = dataset.connect("sqlite:///db/tweets.db")
-    if track is None:
+    if track is None and locations is None:
         return [table.split("__")[1:] for table in database.tables]
+
+    if track is not None and locations is not None:
+        raise ValueError("Cannot filter both track and location")
 
     if session is None:
         session = "{:%Y%m%d-%H%M%S}".format(datetime.now())
-    name = "track__{}__{}".format(track, session)
+
+    if track is not None:
+        name = "track__{}__{}".format(track, session)
+    elif locations is not None:
+        name = "locations__{}__{}".format(locations, session)
 
     return database.get_table(name, primary_id="id_str", primary_type="String")
