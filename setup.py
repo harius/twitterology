0db865610911823e2f64f04970dfa480a7df48ad
Zakhar Pilipchuk <zahaaar@yandex-team.ru>
2016-03-25 10:49:02 +0300

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name="twitterology",
-    version="0.1.8",
+    version="0.1.9",
     url="https://github.com/harius/twitterology",
     description="A set of tools for mining and analyzing data from Twitter",
 
