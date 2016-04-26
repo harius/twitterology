52b21e3de99aa1350815632a210af7535172846a
Zakhar Pilipchuk <zahaaar@yandex-team.ru>
2016-04-26 08:41:40 -0400

    Use Simhash

--- examples/process.py
+++ examples/process.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 # Usage: python examples/process.py track '#HelloWorld' 123456 424242
 from sys import argv
-from re import findall, UNICODE
+from re import findall, split, UNICODE
 
 import twitterology as tw
 
-from datasketch import MinHash, MinHashLSH
 from simhash import Simhash, SimhashIndex
+from collections import Counter
 
 
-def extract_hashtags(text):
-    return findall(r"#\w+", text, UNICODE)
+import logging
+logging.basicConfig(level=logging.ERROR)
+
+
+SEP = u"tWITteroLOgy"
+HASHTAG = r"#\w+"
+
+
+def extract_tweets(texts):
+    return split(SEP, texts, flags=UNICODE)
+
+
+def extract_hashtags_single(tweet):
+    return findall(HASHTAG, tweet, flags=UNICODE)
+
+
+def extract_hashtags(tweets):
+    hashtags = []
+    for tweet in tweets:
+        hashtags.extend(extract_hashtags_single(tweet))
+    return hashtags
+
+
+def extract_features(tweets):
+    hashtags = Counter(extract_hashtags(tweets))
+    return hashtags.most_common(5)
 
 
 def main():
@@ -19,36 +43,45 @@ def main():
     source = tw.sources.tweets(**query)
     storage = tw.sources.timelines(source=source.table.name, session=argv[4])
 
-    response = storage.database.query("""
-        select user__id, group_concat(text) as text
+    users = storage.database.query("""
+        select
+          user__id_str as user_id,
+          group_concat(text, '{sep}') as texts
         from [{storage.table.name}]
-        group by user__id
-    """.format(storage=storage))
+        group by
+          user_id
+    """.format(storage=storage, sep=SEP))
 
-    lsh = MinHashLSH(threshold=0.2, num_perm=128)
-    foo = {}
-    bar = {}
-    chosen = None
-    for user in response:
-        hashtags = extract_hashtags(user["text"])
-        if chosen is None and len(set(hashtags)) > 5:
-            print "chose", user["user__id"]
-            chosen_hashtags = hashtags[::2]
-            hashtags = hashtags[1::2]
-            chosen = MinHash(num_perm=128)
-            for ht in chosen_hashtags:
-                chosen.update(ht.encode("utf-8"))
+    index = SimhashIndex([], k=5)
+    sample_hashes = []
 
-        user_hash = MinHash(num_perm=128)
-        for ht in hashtags:
-            user_hash.update(ht.encode("utf-8"))
+    for user in users:
+        tweets = extract_tweets(user["texts"])
 
-        lsh.insert(user["user__id"], user_hash)
-        foo[user["user__id"]] = user_hash
-        bar[user["user__id"]] = hashtags
+        full_features = extract_features(tweets[::2])
+        sample_features = extract_features(tweets[1::2])
+        if not full_features:
+            continue
 
-    for x in sorted(lsh.query(chosen), key=lambda x: foo[x].jaccard(chosen)):
-        print "candidate", x, foo[x].jaccard(chosen)
+        full_hash = Simhash(full_features)
+        sample_hash = Simhash(sample_features)
+
+        index.add(user["user_id"], full_hash)
+        sample_hashes.append((user["user_id"], sample_hash))
+
+    total = 0
+    successes = 0
+    fails = 0
+    for user_id, sample_hash in sample_hashes:
+        nears = index.get_near_dups(sample_hash)
+
+        total += 1
+        if not nears:
+            fails += 1
+        if len(nears) <= 3 and user_id in nears:
+            successes += 1
+    
+    print successes, "OK,", fails, "FAILS,", total, "TOTAL"
 
 
 if __name__ == "__main__":
